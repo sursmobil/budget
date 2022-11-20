8a649ea80a1811e542d24cd9d2ab641103c2bcf9
Szymon Janota <szymon.janota@pubnub.com>
2022-11-20 14:26:05 +0100

    dummy change

--- packages/budget-ui/src/app/app.tsx
+++ packages/budget-ui/src/app/app.tsx
@@ -5,7 +5,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
-      <NxWelcome title="budget-ui" />
+      <NxWelcome title="budget-ui-test" />
       <div />
     </>
   );
