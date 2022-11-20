78030d232264ae02366097dee162bc47fb5126a0
Szymon Janota <szymon.janota@pubnub.com>
2022-11-20 19:09:13 +0100

    Add basic app layout

--- packages/budget-ui/src/app/app.tsx
+++ packages/budget-ui/src/app/app.tsx
@@ -1,14 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="budget-ui" />
-      <div />
-    </>
-  );
-}
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => (
+  <Layout className="layout">
+    <Header>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['home']}
+        items={[{ label: 'Home', key: 'home' }]}
+      />
+    </Header>
+    <Content className={styles['content']}>
+      <Breadcrumb className={styles['breadcrumb']}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className={styles['site-layout-content']}>Content</div>
+    </Content>
+    <Footer className={styles['footer']}>Karo & Szymo ©2022</Footer>
+  </Layout>
+);
 
 export default App;
